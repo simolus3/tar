0c63561678d072eb2f45c8fcf224e1589d42aece
Simon Binder <oss@simonbinder.eu>
2020-12-24 16:17:44 +0100

    Publish for real

--- lib/tar.dart
+++ lib/tar.dart
@@ -1,5 +1,19 @@
+/// Streaming tar implementation for Dart.
+///
+/// This library is meant to be imported with a prefix:
+///
+/// ```
+/// import 'package:tar/tar.dart' as tar;
+/// ```
+///
+/// To read tar files, see [reader]. To write tar files, use [WritingSink] or
+/// [writer].
 library tar;
 
 export 'src/entry.dart';
 export 'src/reader.dart' show reader;
 export 'src/writer.dart' show WritingSink, writer;
+
+// For dartdoc.
+import 'src/reader.dart';
+import 'src/writer.dart';
